5ad8a0a4d5d326dce0482bea33fb1f6fb3cd9ba7
Adam Campbell <uknet2700@me.com>
2012-04-10 23:34:20 +0100

    Created an array and loop with comments

--- Learning C/C from book/Loop.c
+++ Learning C/C from book/Loop.c
@@ -10,8 +10,11 @@
 
 #include <stdio.h>
 
+// define an array of 5 values
 int values[5] = {12,5,24,56,7};
+//define an integer
 int total = 0;
+//define i as integer for loop counter
 int i;
 
 int main () {
