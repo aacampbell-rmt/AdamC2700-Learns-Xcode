cbfe2a1de4efbbff466c451e9ad79776c41e4a24
Adam Campbell <uknet2700@me.com>
2012-04-12 20:10:39 +0100

    Completed dynamic strings

--- Learning C/C from book/Dynamic Memory and Strings.c
+++ Learning C/C from book/Dynamic Memory and Strings.c
@@ -8,12 +8,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+char* greeting;
 
+int sum();
 int main()
+
 {
     char* FullName;
+    
+    // asprintf = allocate dynamic memory for a string using printf conventions
+    // creates memory block large enough to hold information plus null terminator
     asprintf (&FullName, "Albert Einstein");
     printf("%s \n", FullName);
     
     free(FullName);
+    
+    // include other values using asprintf
+    
+    // declare int and float
+    
+    int total = 81;
+    float ratio = 1.618;
+    
+    // declare a string pointer and format string to reserve dynamic memory
+    
+    char* result;
+    char* resultstart = result;
+    
+    // passes "total: %i. ratio: %f \n" with the total and ratio inserted in string to result variable
+    
+    asprintf(&result, "total: %i. ratio: %f \n", total, ratio);
+    
+    // display the string passed to result variable from asprintf
+    
+    printf("%s \n", result);
+    free(resultstart);
+    
 }
